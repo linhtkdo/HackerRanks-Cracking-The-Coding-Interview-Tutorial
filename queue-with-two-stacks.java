d953ad8f8dbe3315e716d2a237338756289b334e
Linh Do <linhdo@umass.edu>
2016-11-22 15:44:45 -0500

    Update queue-with-two-stacks.java

--- queue-with-two-stacks.java
+++ queue-with-two-stacks.java
@@ -1,2 +1,29 @@
 public class MyQueue<T> {
+  private Stack<T> stackNewestOnTop = new Stack<T>();
+  private Stack<T> stackOldestOnTop = new Stack<T>();
+  
+  public void enqueue(T value) {
+    stackNewestOnTop.push(value);
+  }
+  
+  public T peek() { // get oldest item
+    // move elements from stackNewest to stackOldest
+    shiftStacks();
+    return stackOldestOnTop.peek();
+  }
+  
+  private void shiftStacks() {
+    if (stackOldestOnTop.isEmpty()) 
+      while (!stackNewestOnTop.isEmpty()) 
+        stackOldestOnTop.push(stackNewestOnTop.pop());
+  }
+  
+  public T dequeue() { // get oldest item and remove it
+    // move elements from stackNewest to stackOldest
+    shiftStacks();
+    return stackOldestOnTop.pop(); 
+  }
+}
+      
+
 
