9dd30075be632887cec7e6abcaab6d4f1350d3cb
Linh Do <linhdo@umass.edu>
2016-11-22 16:38:18 -0500

    Update cycles-in-a-linked-list.java

--- cycles-in-a-linked-list.java
+++ cycles-in-a-linked-list.java
@@ -1,1 +1,10 @@
-
+boolean hasCycle(Node head) {
+  if (head == null) return false;
+  Node fast = head.next;
+  while (fast != null && fast.next != null && slow != null) {
+    if (fast == slow) return true;
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return false;
+}
