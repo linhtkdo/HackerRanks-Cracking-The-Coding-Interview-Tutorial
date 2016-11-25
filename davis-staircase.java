390550c90f3e27d718c3dc4a52437eedc6440ed2
Linh Do <linhdo@umass.edu>
2016-11-25 17:29:24 -0500

    Update davis-staircase.java

--- davis-staircase.java
+++ davis-staircase.java
@@ -29,7 +29,7 @@ public static int countPathsDP(int steps) {
   return paths[steps];
 }
 
-public static int countPathsDP(int steps) {
+public static int countPathsI(int steps) {
   if (steps < 0) return 0;
   if (steps <= 1) return 1;
   int[] paths = {1, 1, 2};
