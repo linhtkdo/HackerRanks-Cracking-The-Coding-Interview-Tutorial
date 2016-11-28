86da9b2f754f5152f5af3f117fd841a05e6b8894
Linh Do <linhdo@umass.edu>
2016-11-27 19:42:36 -0500

    Update comparator.java

--- comparator.java
+++ comparator.java
@@ -1,1 +1,14 @@
-
+class Checker implements Comparator<Player> {
+  /* x.compareTo(y):
+        negative value: if x < y (x should appear first)
+        zero if x == y
+        positive value: if x > y (x should appear second(
+  */
+  public int compare(Player a, Player b) {
+    if (a.score == b.score) return a.name.compareTo(b.name);
+    return b.score - a.score; // bigger score comes first
+  }
+}
+      
+  
+  
